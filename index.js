d88d4c5c80ad9f4e973a69fda39107d8d9c648a1
joud awad <joudawad@jouds-MacBook-Pro.local>
2021-08-25 11:19:23 -0400

    adding eslint support to the project

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-"use strict";
-
-const express = require("express");
 // express md to handle markup files
 const expressMd = require("express-md");
-const app = express();
-const port = 8010;
-
-const bodyParser = require("body-parser");
-const jsonParser = bodyParser.json();
 
 const sqlite3 = require("sqlite3").verbose();
+
+const path = require("path");
+
+// initial the app requirement
+const application = require("./src/app");
+
+const port = 8010;
+
 const db = new sqlite3.Database(":memory:");
 
 const buildSchemas = require("./src/schemas");
@@ -17,12 +17,12 @@ const buildSchemas = require("./src/schemas");
 db.serialize(() => {
   buildSchemas(db);
 
-  const app = require("./src/app")(db);
+  const app = application(db);
 
   // create an instance of express-md with custom options
-  var mdRouter = expressMd({
+  const mdRouter = expressMd({
     // serve markdown files from `docs` directory
-    dir: __dirname + "/documentation",
+    dir: path.join(__dirname, "/documentation"),
 
     // serve requests from root of the site
     url: "/",
--- tests/api.test.js
+++ tests/api.test.js
@@ -1,32 +1,32 @@
-'use strict';
+const request = require("supertest");
 
-const request = require('supertest');
+const sqlite3 = require("sqlite3").verbose();
 
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database(':memory:');
+const db = new sqlite3.Database(":memory:");
 
-const app = require('../src/app')(db);
-const buildSchemas = require('../src/schemas');
+const app = require("../src/app")(db);
 
-describe('API tests', () => {
-    before((done) => {
-        db.serialize((err) => { 
-            if (err) {
-                return done(err);
-            }
+const buildSchemas = require("../src/schemas");
 
-            buildSchemas(db);
+describe("API tests", () => {
+  before((done) => {
+    db.serialize((err) => {
+      if (err) {
+        return done(err);
+      }
 
-            done();
-        });
+      buildSchemas(db);
+
+      return done();
     });
+  });
 
-    describe('GET /health', () => {
-        it('should return health', (done) => {
-            request(app)
-                .get('/health')
-                .expect('Content-Type', /text/)
-                .expect(200, done);
-        });
+  describe("GET /health", () => {
+    it("should return health", (done) => {
+      request(app)
+        .get("/health")
+        .expect("Content-Type", /text/)
+        .expect(200, done);
     });
+  });
 });
