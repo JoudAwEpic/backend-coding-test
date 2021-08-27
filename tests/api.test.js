162ab850fe99f12c1604a631978bad52d489cada
joud awad <joudawad@jouds-MacBook-Pro.local>
2021-08-26 21:02:28 -0400

    shipping the test enviroment to use async/await

--- tests/api.test.js
+++ tests/api.test.js
@@ -2,6 +2,8 @@ const request = require("supertest");
 
 const sqlite3 = require("sqlite3").verbose();
 
+const { expect } = require("chai");
+
 const db = new sqlite3.Database(":memory:");
 const app = require("../src/app")(db);
 
@@ -32,97 +34,75 @@ describe("API tests", () => {
   });
 
   describe("GET /rides", () => {
-    it("throw an error when no value returned", (done) => {
-      request(app).get("/rides").expect(500, done);
+    it("throw an error when no value returned", async () => {
+      await request(app).get("/rides").expect(500);
     });
-    it("should return records", (done) => {
-      request(app)
-        .post("/rides")
-        .send(ride)
-        .expect(200)
-        .end((err) => {
-          if (!err) request(app).get("/rides").expect(200, done);
-        });
+    it("should return records", async () => {
+      const result = await request(app).post("/rides").send(ride).expect(200);
+      expect(result.body).to.have.lengthOf(1);
     });
-    it("should work with pagination", (done) => {
-      request(app)
-        .post("/rides")
-        .send(ride)
-        .expect(200)
-        .then(() => {
-          request(app)
-            .post("/rides")
-            .send(ride)
-            .expect(200)
-            .then(() => {
-              request(app)
-                .get(`/rides?page=1&limit=1`)
-                .expect(200)
-                .then(() => {
-                  request(app).get(`/rides?page=2&limit=1`).expect(200);
+    it("should work with pagination", async () => {
+      await request(app).post("/rides").send(ride).expect(200);
+      await request(app).post("/rides").send(ride).expect(200);
+      const firstPage = await request(app)
+        .get(`/rides?page=1&limit=1`)
+        .expect(200);
 
-                  done();
-                });
-            });
-        });
+      expect(firstPage.body.next_page).to.equal(true);
     });
   });
 
   describe("POST /rides", () => {
-    it("should return a created record", (done) => {
-      request(app)
+    it("should return a created record", async () => {
+      const result = await request(app)
         .post("/rides")
         .send(ride)
         .expect("Content-Type", "application/json; charset=utf-8")
-        .expect(200, done);
+        .expect(200);
+
+      expect(result.body).to.have.lengthOf(1);
     });
-    it("should throw an error when rider_name is missing", (done) => {
-      request(app).post("/rides").send(errorRiderName).expect(400, done);
+    it("should throw an error when rider_name is missing", async () => {
+      await request(app).post("/rides").send(errorRiderName).expect(400);
     });
 
-    it("should throw an error when driver_name is missing", (done) => {
-      request(app).post("/rides").send(errorDriverName).expect(400, done);
+    it("should throw an error when driver_name is missing", async () => {
+      await request(app).post("/rides").send(errorDriverName).expect(400);
     });
 
-    it("should throw an error when driver_vehicle is missing", (done) => {
-      request(app).post("/rides").send(errorDriverVehicle).expect(400, done);
+    it("should throw an error when driver_vehicle is missing", async () => {
+      await request(app).post("/rides").send(errorDriverVehicle).expect(400);
     });
 
-    it("should throw an error when startLatitude is not in the right range", (done) => {
-      request(app).post("/rides").send(errorStartLat).expect(400);
-      done();
+    it("should throw an error when startLatitude is not in the right range", async () => {
+      await request(app).post("/rides").send(errorStartLat).expect(400);
     });
 
-    it("should throw an error when startLongitude is not in the right range", (done) => {
-      request(app).post("/rides").send(errorStartLong).expect(400, done);
+    it("should throw an error when startLongitude is not in the right range", async () => {
+      await request(app).post("/rides").send(errorStartLong).expect(400);
     });
 
-    it("should throw an error when endLatitude is not in the right range", (done) => {
-      request(app).post("/rides").send(errorEndLatitude).expect(400, done);
+    it("should throw an error when endLatitude is not in the right range", async () => {
+      await request(app).post("/rides").send(errorEndLatitude).expect(400);
     });
   });
 
   describe("GET /rides/:id", () => {
-    it("should return a record", (done) => {
-      request(app)
-        .post("/rides")
-        .send(ride)
-        .expect(200)
-        .end((err) => {
-          if (!err) request(app).get(`/rides/1`).expect(200, done);
-        });
+    it("should return a record", async () => {
+      const result = await request(app).post("/rides").send(ride).expect(200);
+      await request(app).get(`/rides/${result.body[0].rideID}`).expect(200);
     });
-    it("should throw an error when no record found", (done) => {
-      request(app).get("/rides/1231141321").expect(404, done);
+    it("should throw an error when no record found", async () => {
+      await request(app).get("/rides/1231141321").expect(404);
     });
   });
 
   describe("GET /health", () => {
-    it("should return health", (done) => {
-      request(app)
+    it("should return health", async () => {
+      await request(app)
         .get("/health")
         .expect("Content-Type", /text/)
-        .expect(200, done);
+        .expect(200);
     });
   });
 });
